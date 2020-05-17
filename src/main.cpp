5ad1ad2a22eaa268235972dd3b91e1cf5aacb7d1
unknown <blaine.rieger@gmail.com>
2020-05-17 12:42:48 -0700

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-// the setup function runs once when you press reset or power the board
+#include <Adafruit_NeoPixel.h>
+
+#define PIN        3 // On Trinket or Gemma, suggest changing this to 1
+
+#define NUMPIXELS 30 // Popular NeoPixel ring size
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);         //start the serial communication
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+  }
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
+
+
+  if (Serial.available() >= 3) {         //if some data is available of in the serial port
+    byte r = Serial.read();
+    byte g = Serial.read();
+    byte b = Serial.read();
+
+    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+      pixels.setPixelColor(i, pixels.Color(int(r), int(g), int(b)));
+    }
+    pixels.show();   // Send the updated pixel colors to the hardware.
+    Serial.flush();
+  }
+
+
 }
